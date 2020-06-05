e1fd4cb3151d9d5e7cdc9b27bb243fc210cb7d9d
soyalatte <soyalatte@neowiz.com>
2020-06-05 20:01:20 +0900

    컴포넌트 구조 분리(Header,Content,Footer)

---
+++ README.md
@@ -0,0 +1,6 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## mSNS
+
+map SNS 웹사이트 
+react 기반 typescript 로 작성 
---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+import './App.css';
+
+interface AppState{
+  loading : boolean;
+}
+
+export interface IMapAPI {
+  save: (title: string) => Promise<void>;
+  getAll: () => Promise<void>; 
+  remove: (id: number) => Promise<void>; 
+  toggle: (id: number, completed: boolean) => Promise<void>;
+};
+
+class App extends React.Component<{},AppState> {
+  constructor(props: any){
+    super(props);
+    this.state = {
+      loading : false
+    };
+  }
+
+  get mapAPI(){
+    return{
+      save: this.saveArticle.bind(this),
+      getAll: this.fetchArticles.bind(this),
+      remove: this.removeArticle.bind(this),
+      toggle: this.toggleArticle.bind(this),
+    }
+  }
+
+  componentDidMount(){
+      this.fetchArticles();
+
+  }
+
+  saveArticle = (title: string)=> {
+    
+  }
+
+  toggleArticle = (id: number, completed: boolean)=> {
+  
+  }
+
+  removeArticle = (id: number)=> {
+    
+  }
+
+  fetchArticles = ()=> {
+    
+  }
+
+  public render(){
+    return (
+      <div className="map-app">
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
+
